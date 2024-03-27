0d40f3d696b52c311a0e04f9d10215adf0788ddd
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2024-03-27 03:18:16 +0000

    Test Jar

---
+++ src/main/java/org/isep/Utils/Dummy.java
@@ -0,0 +1,12 @@
+package org.isep.Utils;
+
+public class Dummy {
+
+    private int num1 = 2;
+    private int num2 = 5;
+
+
+    public int add() {
+        return num1 + num2;
+    }
+}
---
+++ src/test/java/org/isep/Utils/DummyTest.java
@@ -0,0 +1,14 @@
+package org.isep.Utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DummyTest {
+
+    @Test
+    void add() {
+        int sum = 4+5;
+        assertEquals(sum, 9);
+    }
+}
